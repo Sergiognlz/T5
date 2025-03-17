9b95400b7d2e6fddb5d75a1efb4a98904d024820
sgonzalez <sgonzalez@106-03.info2>
2025-03-17 12:44:06 +0100

    4

---
+++ Tema5/src/parte1/ejercicio4/PrincipalElec.java
@@ -0,0 +1,12 @@
+package parte1.ejercicio4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrincipalElec {
+	//creamos lista electrodomésticos
+	List<Electrodomestico>listaElec=new ArrayList<>();
+	
+	
+	
+}
