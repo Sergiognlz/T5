843f6c6b50c6cf655b1e8d8d1f851aaf906551f7
sgonzalez <sgonzalez@106-03.info2>
2025-03-24 12:41:03 +0100

    2

---
+++ Tema5/src/interfaces/ejercicio2/Futbolista.java
@@ -0,0 +1,107 @@
+package interfaces.ejercicio2;
+
+/**
+ * Clase futbolista con sus datos implementando la interfaz Comparable de tipo Futbolista
+ */
+public class Futbolista implements Comparable<Futbolista> {
+
+	/**
+	 * atributo privado número de camiseta
+	 */
+	private int numCamiseta;
+	/**
+	 * atributo privato nombre de futbolista
+	 */
+	private String nombre;
+	/**
+	 * atributo privado edad de futbolista
+	 */
+	private int edad;
+	/**
+	 * atributo privado número de goles del futbolista
+	 */
+	private int numGoles;
+
+	/**
+	 * constructor futbolista
+	 * 
+	 * @param numCamiseta
+	 * @param nombre
+	 */
+	public Futbolista(int numCamiseta, String nombre) {
+		if (numCamiseta > 0) {
+			this.numCamiseta = numCamiseta;
+		}
+		if (nombre != null && !nombre.isBlank()) {
+			this.nombre = nombre;
+		}
+	}
+
+	/**
+	 * constructor completo futbolista
+	 * 
+	 * @param numCamiseta
+	 * @param nombre
+	 * @param edad
+	 * @param numGoles
+	 */
+	public Futbolista(int numCamiseta, String nombre, int edad, int numGoles) {
+		if (numCamiseta > 0) {
+			this.numCamiseta = numCamiseta;
+		}
+		if (nombre != null && !nombre.isBlank()) {
+			this.nombre = nombre;
+		}
+		if (edad >= 16) {
+			this.edad = edad;
+		}
+		if (numGoles >= 0) {
+			this.numGoles = numGoles;
+		}
+	}
+
+	@Override
+	public int compareTo(Futbolista obj) {
+		// variable respuesta igual al número de la camiseta del objeto menos el número
+		// que la camiseta con el que comparamos
+		int res = this.numCamiseta - obj.numCamiseta;
+		// si los números son iguales
+		if (this.numCamiseta == obj.numCamiseta) {
+			// hacemos una comparación de los nombres
+			res = this.nombre.compareTo(obj.nombre);
+		}
+		// devolvemos respuesta
+		return res;
+	}
+
+	/**
+	 * Sobreescritura del método toString
+	 */
+	@Override
+	public String toString() {
+		// variable cadena
+		String cadena = "Nombre: " + this.nombre + " Nº Camiseta: " + this.numCamiseta + "\nEdad: " + this.edad
+				+ " Nº Goles: " + this.numGoles;
+		// devolvemos cadena
+		return cadena;
+	}
+
+	/**
+	 * sobreescritura de equals
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		// variable éxito
+		boolean exito = false;
+		// creamos objeto futbolista
+		Futbolista fut = (Futbolista) obj;
+		// si dos futbolistas coinciden en número de camiseta y nombre serán el mismo
+		if (this.numCamiseta == fut.numCamiseta && this.nombre.equals(fut.nombre)) {
+			// éxito será true
+			exito = true;
+		}
+		//devolvemos éxito
+		return exito;
+	}
+
+}
---
+++ Tema5/src/interfaces/ejercicio2/PrincipalFutbolista.java
@@ -0,0 +1,32 @@
+package interfaces.ejercicio2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PrincipalFutbolista {
+	public static void main(String[] args) {
+		// creamos lista de arrays de tipo futbolista
+		List<Futbolista> listaFutbolistas = new ArrayList<>();
+		// creamos objetos futbolistas
+		Futbolista fut1 = new Futbolista(1, "Paco Merla", 28, 5);
+		Futbolista fut2 = new Futbolista(2, "Elver Galarga", 30, 10);
+		Futbolista fut3 = new Futbolista(3, "María Unpajote", 18, 0);
+		Futbolista fut4 = new Futbolista(4, "Elva Ginón", 22, 0);
+		Futbolista fut5 = new Futbolista(5, "Tomás Turbado", 40, 2);
+		// añadimos los futbolistas
+		listaFutbolistas.add(fut1);
+		listaFutbolistas.add(fut2);
+		listaFutbolistas.add(fut3);
+		listaFutbolistas.add(fut4);
+		listaFutbolistas.add(fut5);
+		//ordenamos lista
+		Collections.sort(listaFutbolistas);
+		//recorremos la lista con un for each
+		for(Futbolista fut:listaFutbolistas) {
+			//imprimimos futbolistas
+			System.out.println(fut);
+			System.out.println();
+		}
+	}
+}
