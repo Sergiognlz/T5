c92f19950c64508131088ad6d8e01dbdc1e0168f
Sergio <Sergio@192.168.1.134>
2025-03-31 20:27:38 +0200

    main lastimoso

---
+++ Tema5/src/analisis/ejercicio3/PrincipalCuentaBancaria.java
@@ -0,0 +1,15 @@
+package analisis.ejercicio3;
+
+public class PrincipalCuentaBancaria {
+public static void main(String[] args) {
+	
+	Titular t1=new Titular("12345678L","Paco","Merla","+95451070");
+	CuentaBancaria c1=new CuentaBancaria("ES10010101001", t1);
+	
+	
+	
+	
+	
+	
+}
+}
